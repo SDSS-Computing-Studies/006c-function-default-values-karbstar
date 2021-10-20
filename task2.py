50ccd67dbe2ef5298e41bc6ce636cc7dbefabddb
karbstar <90715867+karbstar@users.noreply.github.com>
2021-10-20 14:52:24 -0700

    sf

--- task2.py
+++ task2.py
@@ -24,12 +24,17 @@ def multiplication(x,y=12):
         g=x*z
         z=z+1
     
-        print(g,z)
         
-        if u==y:
+        if z==y:
+            lst.append(g)
+            g=x*z
+            lst.append(g)
+            print(lst)
             d=False
         else:
+
             lst.append(g)
+            print(lst)
     return lst
 
 assert multiplication(5) == [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
